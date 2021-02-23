91065b2dfaa2bf81d9eff8f7e5ebe31fc0377ed9
Nishant Goyal <nishant.goyal0789@gmail.com>
2021-02-23 10:39:18 +0530

    completed section 03 lecture 06

---
+++ Java Programming Masterclass for Software Developers/Sections/03. First Steps/src/sumNumbers.java
@@ -0,0 +1,11 @@
+public class sumNumbers {
+  public static void main(String[] args) {
+    System.out.println("Hello, Nishant");
+    int myFirstNumber = (10 + 5) + (2 * 10);
+    int mySecondNumber = 12;
+    int myThirdNumber = myFirstNumber * 2;
+    // int myThirdNumber = 6;
+    int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+    System.out.println(myTotal);
+  }
+}
