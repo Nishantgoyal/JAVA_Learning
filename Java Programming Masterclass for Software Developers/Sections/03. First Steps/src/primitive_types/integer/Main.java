40d9468a4a0d577f1fd9dbb86ffdba27a2055f71
Nishant Goyal <nishant.goyal0789@gmail.com>
2021-02-24 13:41:42 +0530

    completed section 03 lecture 07

---
+++ Java Programming Masterclass for Software Developers/Sections/03. First Steps/src/primitive_types/integer/Main.java
@@ -0,0 +1,16 @@
+package primitive_types.integer;
+
+public class Main {
+  public static void main(String[] args) {
+
+    // Integer
+    int myMinIntValue = Integer.MIN_VALUE;
+    int myMaxIntValue = Integer.MAX_VALUE;
+    System.out.println("Integer Min Value: " + myMinIntValue);
+    System.out.println("Integer Max Value: " + myMaxIntValue);
+    System.out.println("Busted Max Value: " + (myMaxIntValue + 1));
+    System.out.println("Busted Min Value: " + (myMinIntValue - 1)); // --> -2147483648
+
+    // int myMaxIntTest = 2147483648;
+  }
+}
